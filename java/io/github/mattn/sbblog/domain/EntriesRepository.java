b43fc2d83724475bf15c82d3ff9d46768c5f3609
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-08-22 00:42:45 +0900

    dos2unix

--- java/io/github/mattn/sbblog/domain/EntriesRepository.java
+++ java/io/github/mattn/sbblog/domain/EntriesRepository.java
@@ -1,6 +1,8 @@
 package io.github.mattn.sbblog.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EntriesRepository extends JpaRepository<Entry, Integer> {
 }
--- src/test/java/io/github/mattn/sbblog/ApplicationTests.java
+++ src/test/java/io/github/mattn/sbblog/ApplicationTests.java
@@ -5,8 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringRunner.class)
+@WebAppConfiguration
 @ContextConfiguration(classes = Application.class)
 @SpringBootTest
 public class ApplicationTests {
